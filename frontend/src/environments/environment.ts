213053705bdf9748f3c40830cf8b3e4f9d95837c
MonicaSai7 <monica.sai7@gmail.com>
2021-06-05 11:05:48 +0530

    Successful installation

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'cafe-sky.us', // the auth0 domain prefix
+    audience: 'cafe', // the audience set for the auth0 app
+    clientId: 'MM2K8zHkJvc2PTqtVeKVyJy2hp8l6sVP', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
